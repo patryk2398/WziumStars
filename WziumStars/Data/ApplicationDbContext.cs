44e6e75efa4131e9cba74ce93fd7b682c46a010a
Patryk Jaworski <patryk.jaworski23@gmail.com>
2020-10-20 17:42:35 +0200

    Add "Podkategoria" model

--- WziumStars/Data/ApplicationDbContext.cs
+++ WziumStars/Data/ApplicationDbContext.cs
@@ -15,5 +15,6 @@ namespace WziumStars.Data
         }
 
         public DbSet<Kategoria> Kategoria { get; set; }
+        public DbSet<Podkategoria> Podkategoria { get; set; }
     }
 }
---
+++ WziumStars/Data/Migrations/20201020154149_addPodkategoriaToDb.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WziumStars.Data.Migrations
+{
+    public partial class addPodkategoriaToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Podkategoria",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    CategoryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Podkategoria", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Podkategoria_Kategoria_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Kategoria",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Podkategoria_CategoryId",
+                table: "Podkategoria",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Podkategoria");
+        }
+    }
+}
