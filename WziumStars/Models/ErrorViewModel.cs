f673f82b0a8ff3887afad72f64f2ff1d53e314da
Patryk Jaworski <patryk.jaworski23@gmail.com>
2020-10-20 14:49:14 +0200

    Dodaj pliki projektów.

---
+++ WziumStars/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WziumStars.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
