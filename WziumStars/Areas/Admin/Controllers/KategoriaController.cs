b1e0cdc105cb512958b3408e14f1ca23b4453b82
Patryk Jaworski <patryk.jaworski23@gmail.com>
2020-10-20 15:26:15 +0200

    Create Index view for Kategoria

---
+++ WziumStars/Areas/Admin/Controllers/KategoriaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WziumStars.Data;
+
+namespace WziumStars.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class KategoriaController : Controller
+    {
+        public readonly ApplicationDbContext _db;
+
+        public KategoriaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //GET
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Kategoria.ToListAsync());
+        }
+
+
+    }
+}
