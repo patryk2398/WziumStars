fffe087de387d3492000bb6b4035ce4f0e08ee33
Patryk Jaworski <patryk.jaworski23@gmail.com>
2020-10-20 20:09:32 +0200

    Create "Edytuj" "Podkategoria" methods and view

--- WziumStars/Areas/Admin/Controllers/PodkategoriaController.cs
+++ WziumStars/Areas/Admin/Controllers/PodkategoriaController.cs
@@ -24,14 +24,14 @@ namespace WziumStars.Areas.Admin.Controllers
             _db = db;
         }
 
-        //GET
+        //GET - INDEX
         public async Task<IActionResult> Index()
         {
             var podkategorie = await _db.Podkategoria.Include(s=>s.Kategoria).ToListAsync();
             return View(podkategorie);
         }
 
-        //GET
+        //GET - CREATE
         public async Task<IActionResult> Dodaj()
         {
             PodkategoriaAndKategoriaViewModel model = new PodkategoriaAndKategoriaViewModel()
@@ -43,7 +43,7 @@ namespace WziumStars.Areas.Admin.Controllers
             return View(model);
         }
 
-        //POST
+        //POST - CREATE
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Dodaj(PodkategoriaAndKategoriaViewModel model)
@@ -82,6 +82,61 @@ namespace WziumStars.Areas.Admin.Controllers
                             where podkategoria.CategoryId == id
                             select podkategoria).ToListAsync();
             return Json(new SelectList(podkategorie, "Id", "Name"));
+        }
+
+        //GET - EDIT
+        public async Task<IActionResult> Edytuj(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+
+            var podkategoria = await _db.Podkategoria.SingleOrDefaultAsync(m => m.Id == id);
+
+            if(podkategoria==null)
+            {
+                return NotFound();
+            }
+
+            PodkategoriaAndKategoriaViewModel model = new PodkategoriaAndKategoriaViewModel()
+            {
+                KategoriaList = await _db.Kategoria.ToListAsync(),
+                Podkategoria = podkategoria,
+                PodkategoriaList = await _db.Podkategoria.OrderBy(p => p.Name).Select(p => p.Name).Distinct().ToListAsync()
+            };
+            return View(model);
+        }
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edytuj(int id, PodkategoriaAndKategoriaViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var doesPodkategoriaExist = _db.Podkategoria.Include(s => s.Kategoria).Where(s => s.Name == model.Podkategoria.Name && s.Kategoria.Id == model.Podkategoria.CategoryId);
+                if (doesPodkategoriaExist.Count() > 0)
+                {
+                    StatusMessage = "Error: Podkategoria " + doesPodkategoriaExist.First().Name + " istenieje już w kategorii " + doesPodkategoriaExist.First().Kategoria.Name;
+                }
+                else
+                {
+                    var podkategoriaFromDb = await _db.Podkategoria.FindAsync(id);
+                    podkategoriaFromDb.Name = model.Podkategoria.Name;
+                    await _db.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            PodkategoriaAndKategoriaViewModel modelVM = new PodkategoriaAndKategoriaViewModel()
+            {
+                KategoriaList = await _db.Kategoria.ToListAsync(),
+                Podkategoria = model.Podkategoria,
+                PodkategoriaList = await _db.Podkategoria.OrderBy(p => p.Name).Select(p => p.Name).ToListAsync(),
+                StatusMessage = StatusMessage
+            };
+            modelVM.Podkategoria.Id = id;
+            return View(modelVM);
         }
     }
 }
