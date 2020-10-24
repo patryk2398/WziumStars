45c00d57b9032635e367636724a721f1dae96d68
Patryk Jaworski <patryk.jaworski23@gmail.com>
2020-10-24 18:03:12 +0200

    Create "Edytuj" methods and view for "Produkt"

--- WziumStars/Areas/Klient/Controllers/ProduktController.cs
+++ WziumStars/Areas/Klient/Controllers/ProduktController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WziumStars.Data;
+using WziumStars.Models;
 using WziumStars.Models.ViewModels;
 using WziumStars.Utility;
 
@@ -48,6 +50,8 @@ namespace WziumStars.Areas.Klient.Controllers
         [HttpPost, ActionName("Dodaj")]
         public async Task<IActionResult> DodajPOST()
         {
+            ProduktVM.Produkt.SubCategoryId = Convert.ToInt32(Request.Form["SubCategoryId"].ToString());
+            int test = Convert.ToInt32(Request.Form["SubCategoryId"].ToString());
             if (!ModelState.IsValid)
             {
                 return View();
@@ -63,19 +67,19 @@ namespace WziumStars.Areas.Klient.Controllers
             if (files.Count > 0)
             {
                 string[] imagesTable = new string[files.Count];
-                for(int i = 0; i < files.Count; i++)
+                for (int i = 0; i < files.Count; i++)
                 {
-                    var uploads = Path.Combine(webRootPath, "img");
+                    var uploads = Path.Combine(webRootPath, "imgProdukt");
                     var extension = Path.GetExtension(files[i].FileName);
 
-                    using (var fileStream = new FileStream(Path.Combine(uploads, ProduktVM.Produkt.Id + "."+ i + extension), FileMode.Create))
+                    using (var fileStream = new FileStream(Path.Combine(uploads, ProduktVM.Produkt.Id + "." + i + extension), FileMode.Create))
                     {
                         files[i].CopyTo(fileStream);
                     }
-                    imagesTable[i] = @"\img\" + ProduktVM.Produkt.Id + "." + i + extension;
+                    imagesTable[i] = @"\imgProdukt\" + ProduktVM.Produkt.Id + "." + i + extension;
                 }
                 StringBuilder allImages = new StringBuilder();
-                for(int i = 0; i < files.Count; i++)
+                for (int i = 0; i < files.Count; i++)
                 {
                     allImages.Append(imagesTable[i]);
                     allImages.Append("|");
@@ -86,10 +90,97 @@ namespace WziumStars.Areas.Klient.Controllers
             else
             {
                 var uploads = Path.Combine(webRootPath, @"images\" + SD.DefaultProduktImage);
-                System.IO.File.Copy(uploads, webRootPath + @"\img\" + ProduktVM.Produkt.Id + ".png");
-                produktFromDb.Images = @"\img\" + ProduktVM.Produkt.Id + ".png";
-                produktFromDb.Avatar = @"\img\" + ProduktVM.Produkt.Id + ".png";
+                System.IO.File.Copy(uploads, webRootPath + @"\imgProdukt\" + ProduktVM.Produkt.Id + ".png");
+                produktFromDb.Images = @"\imgProdukt\" + ProduktVM.Produkt.Id + ".png";
+                produktFromDb.Avatar = @"\imgProdukt\" + ProduktVM.Produkt.Id + ".png";
             }
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        //EDIT - GET
+        public async Task<IActionResult> Edytuj(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ProduktVM.Produkt = await _db.Produkt.Include(m => m.Category).Include(m => m.SubCategory).SingleOrDefaultAsync(m => m.Id == id);
+            ProduktVM.Podkategoria = await _db.Podkategoria.Where(s => s.CategoryId == ProduktVM.Produkt.CategoryId).ToListAsync();
+            
+            if(ProduktVM.Produkt == null)
+            {
+                return NotFound();
+            }
+
+            return View(ProduktVM);
+        }
+
+        //EDIT - POST
+        [HttpPost, ActionName("Edytuj")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EdytujPOST(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ProduktVM.Produkt.SubCategoryId = Convert.ToInt32(Request.Form["SubCategoryId"].ToString());
+
+            if (!ModelState.IsValid)
+            {
+                ProduktVM.Podkategoria = await _db.Podkategoria.Where(s => s.CategoryId == ProduktVM.Produkt.CategoryId).ToListAsync();
+                return View(ProduktVM);
+            }
+
+            string webRootPath = _webHostEnvironment.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+            var ProduktFromDb = await _db.Produkt.FindAsync(ProduktVM.Produkt.Id);
+
+            if (files.Count > 0)
+            {
+                var uploads = Path.Combine(webRootPath, "imgProdukt");
+                int count = ProduktFromDb.Images.Count(f => f == '|');
+                for (int i = 0; i < count; i++)
+                {
+                    string[] array = ProduktFromDb.Images.Split('|');
+                    var imagePath = Path.Combine(webRootPath, array[i].TrimStart('\\'));
+                    if (System.IO.File.Exists(imagePath))
+                    {
+                        System.IO.File.Delete(imagePath);
+                    }
+                }
+
+                string[] imagesTable = new string[files.Count];
+                for (int i = 0; i < files.Count; i++)
+                {
+                    var extension = Path.GetExtension(files[i].FileName);
+
+                    using (var fileStream = new FileStream(Path.Combine(uploads, ProduktVM.Produkt.Id + "." + i + extension), FileMode.Create))
+                    {
+                        await files[i].CopyToAsync(fileStream);
+                    }
+                    imagesTable[i] = @"\imgProdukt\" + ProduktVM.Produkt.Id + "." + i + extension;
+                }
+                StringBuilder allImages = new StringBuilder();
+                for (int i = 0; i < files.Count; i++)
+                {
+                    allImages.Append(imagesTable[i]);
+                    allImages.Append("|");
+                }
+                ProduktFromDb.Images = allImages.ToString();
+                ProduktFromDb.Avatar = imagesTable[0];
+            }
+
+            ProduktFromDb.Name = ProduktVM.Produkt.Name;
+            ProduktFromDb.Description = ProduktVM.Produkt.Description;
+            ProduktFromDb.Price = ProduktVM.Produkt.Price;
+            ProduktFromDb.CategoryId = ProduktVM.Produkt.CategoryId;
+            ProduktFromDb.SubCategoryId = ProduktVM.Produkt.SubCategoryId;
+
             await _db.SaveChangesAsync();
 
             return RedirectToAction(nameof(Index));
